329af2e76aaa4d452310d2f1d1c9b65eb1389457
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-06 12:04:07 -0500

    database connection

--- server.js
+++ server.js
@@ -6,19 +6,42 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 /////////////////////////////////////////////////
 // Create our Express Application Object
 /////////////////////////////////////////////////
 const app = express()
 
+/////////////////////////////////////////////////
+// Mongoose Connection
+/////////////////////////////////////////////////
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
 /////////////////////////////////////////////////////
 // Middleware
 /////////////////////////////////////////////////////
 app.use(morgan("tiny"))
 app.use(methodOverride("_method"))
-app.use(express.static("public"))
+app.use("/static", express.static("public"))
+
+/////////////////////////////////////////////////////
+// Routes and Routers
+/////////////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("Server is working")
+})
 
 
 //////////////////////////////////////////////
