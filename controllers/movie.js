a81ca92b7d58795e6d258f2a49fe9402d0332d95
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-10 13:43:56 -0500

    revert back to MVP

--- controllers/movie.js
+++ controllers/movie.js
@@ -23,45 +23,25 @@ function errorHandler(error, res){
 // Seed Router
 router.get("/seed",  async (req, res) => {
     const startMovies = [
-        {title: "Titanic", notes: "I loved it!", isLiked: true},
-        {title: "Eternal Sunshine of the Spotless Mind", notes: "Deep!", isLiked: true},
-        {title: "Terrifier 2", notes: "too gory!", isLiked: false},
+        {title: "Titanic", notes: "I loved it!", genre: "Romance", isLiked: true},
+        {title: "Eternal Sunshine of the Spotless Mind", notes: "Deep!", genre: "Drama", isLiked: true},
+        {title: "Terrifier 2", notes: "too gory!", isLiked: false, genre: "Horror"},
     ]
-
-    const startGenres = [
-        {name: "Romance"},
-        {name: "Drama"},
-        {name: "Horror"}
-    ]
+    Movie.deleteMany({}, (error, data) => {})
     const newMovies = await Movie.create(startMovies)
-    const newGenres = await Genre.create(startGenres)
-    res.json({newMovies, newGenres})
+    res.json({newMovies})
 })
 
-// Link Route
-router.get("/linkmovies/:genreid/:movieid", async (req, res) => {
-    req.params.genreid = toId(req.params.genreid)
-    const movie = await Movie.findById(req.params.movieid)
-    movie.genre = req.params.genreid
-    movie.save()
-    res.json(movie)
-})
 
 // Home Page
 router.get("/home", (req, res) => {
     res.render("movie/home.ejs")
 })
 
-// Genres Page
-router.get("/genres", async (req, res) => {
-    const movies = await Movie.find({}).catch((error) => {errorHandler(error, res)})
-    const genres = await Genre.find({}).populate({path: "movie", model: "Movie"}).catch((error) => errorHandler(error, res))
-    res.render("movie/genre.ejs", {movies, genres})
-})
 
 // Index Route 
 router.get("/", async (req, res) => {
-    const movies = await Movie.find({}).populate({path: "movie", model: "Movie"}).catch((error) => errorHandler(error, res))
+    const movies = await Movie.find({}).catch((error) => errorHandler(error, res))
     res.render("movie/index.ejs", {movies})
 })
 
--- models/movie.js
+++ models/movie.js
@@ -8,24 +8,19 @@ const mongoose = require("./connection")
 ////////////////////////////////////////////////
 const {Schema, model} = mongoose // destructuring
 
-// Make genre schema
-const genreSchema = new Schema ({
-    name: String,
-    movies: [{type: mongoose.Types.ObjectId, ref:"Movie"}]
-})
-
 // Make movie schema
 const movieSchema = new Schema ({
     title: String,
     notes: String,
-    genre: genreSchema,
+    isLiked: Boolean,
+    genre: String
 }, {timestamps: true})
+
 
 // Make movie model
 const Movie = model("Movie", movieSchema)
-const Genre = model("Genre", genreSchema)
 
 ///////////////////////////////////////////////////
 // Export Model
 ///////////////////////////////////////////////////
-module.exports = {Movie, Genre}
+module.exports = {Movie}
